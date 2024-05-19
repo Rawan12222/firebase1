35359f414a2a0e40231a48995de50c5b4c0671fb
Rawan <rawanaldawood08@gmail.com>
2024-05-20 02:13:54 +0300

    adding files

---
+++ lib/widget_tree.dart
@@ -0,0 +1,29 @@
+import 'package:firebase1/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase1/home_page.dart';
+import 'login_register_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return  StreamBuilder(
+      stream: Auth().authStateChange,
+      builder: (context , snapshot){
+        if (snapshot.hasData){
+          return HomePage();
+        }else{
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
